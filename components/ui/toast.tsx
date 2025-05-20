9dd0620c1006fc8f7e37bd90421aafd4a52f9506
VoyagerNgoc <ngocph@voyager-hcm.com>
2025-05-20 10:00:21 +0700

    update UI

--- components/ui/toast.tsx
+++ components/ui/toast.tsx
@@ -23,24 +23,25 @@ const ToastViewport = React.forwardRef<
 ToastViewport.displayName = ToastPrimitives.Viewport.displayName
 
 const toastVariants = cva(
-  "group pointer-events-auto relative flex w-full items-center justify-between space-x-4 overflow-hidden rounded-md border p-6 pr-8 shadow-lg transition-all data-[swipe=cancel]:translate-x-0 data-[swipe=end]:translate-x-[var(--radix-toast-swipe-end-x)] data-[swipe=move]:translate-x-[var(--radix-toast-swipe-move-x)] data-[swipe=move]:transition-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[swipe=end]:animate-out data-[state=closed]:fade-out-80 data-[state=closed]:slide-out-to-right-full data-[state=open]:slide-in-from-top-full",
+  "group pointer-events-auto relative flex w-full items-center justify-between space-x-4 overflow-hidden rounded-md border p-6 pr-8 shadow-lg transition-all data-[swipe=cancel]:translate-x-0 data-[swipe=end]:translate-x-[var(--radix-toast-swipe-end-x)] data-[swipe=move]:translate-x-[var(--radix-toast-swipe-move-x)] data-[swipe=move]:transition-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[swipe=end]:animate-out data-[state=closed]:fade-out-80 data-[state=closed]:slide-out-to-right-full data-[state=open]:slide-in-from-top-full text-base font-semibold",
   {
     variants: {
       variant: {
         default: "border bg-background text-foreground",
         destructive:
-          "border-red-500 bg-red-500/15 text-red-600 dark:border-red-900 dark:bg-red-950/50 dark:text-red-400",
+          "border-red-500 bg-red-100 text-red-900 dark:border-red-900 dark:bg-red-950 dark:text-red-200",
         success:
-          "border-green-500 bg-green-500/15 text-green-600 dark:border-green-900 dark:bg-green-950/50 dark:text-green-400",
-        info: "border-blue-500 bg-blue-500/15 text-blue-600 dark:border-blue-900 dark:bg-blue-950/50 dark:text-blue-400",
+          "border-green-500 bg-green-100 text-green-900 dark:border-green-900 dark:bg-green-950 dark:text-green-200",
+        info:
+          "border-blue-500 bg-blue-100 text-blue-900 dark:border-blue-900 dark:bg-blue-950 dark:text-blue-200",
         warning:
-          "border-yellow-500 bg-yellow-500/15 text-yellow-600 dark:border-yellow-900 dark:bg-yellow-950/50 dark:text-yellow-400",
+          "border-yellow-500 bg-yellow-100 text-yellow-900 dark:border-yellow-900 dark:bg-yellow-950 dark:text-yellow-200",
       },
     },
     defaultVariants: {
       variant: "default",
     },
-  },
+  }
 )
 
 const Toast = React.forwardRef<
@@ -88,7 +89,7 @@ const ToastTitle = React.forwardRef<
   React.ElementRef<typeof ToastPrimitives.Title>,
   React.ComponentPropsWithoutRef<typeof ToastPrimitives.Title>
 >(({ className, ...props }, ref) => (
-  <ToastPrimitives.Title ref={ref} className={cn("text-sm font-semibold", className)} {...props} />
+  <ToastPrimitives.Title ref={ref} className={cn("text-lg font-bold", className)} {...props} />
 ))
 ToastTitle.displayName = ToastPrimitives.Title.displayName
 
@@ -96,7 +97,7 @@ const ToastDescription = React.forwardRef<
   React.ElementRef<typeof ToastPrimitives.Description>,
   React.ComponentPropsWithoutRef<typeof ToastPrimitives.Description>
 >(({ className, ...props }, ref) => (
-  <ToastPrimitives.Description ref={ref} className={cn("text-sm opacity-90", className)} {...props} />
+  <ToastPrimitives.Description ref={ref} className={cn("text-base opacity-95", className)} {...props} />
 ))
 ToastDescription.displayName = ToastPrimitives.Description.displayName
 
